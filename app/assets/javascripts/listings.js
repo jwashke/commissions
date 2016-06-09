785ea5203fa326b29f6a3d72b2af4e96c4bbb6f6
Josh Washke <jwashke@icloud.com>
2016-06-09 14:57:05 -0600

    change pin color on google map

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -1,4 +1,4 @@
-
+var infoWindow
 var mapDiv
 var map
 function initMap() {
@@ -8,19 +8,55 @@ function initMap() {
     zoom: 12
   });
 
+  var contentString = ""
+
+  function getInfoWindowString(address, list_price) {
+    contentString = '<div id="content">'+
+       '<div id="siteNotice">'+
+       '</div>'+
+       '<h1 id="firstHeading" class="firstHeading">' + address + '</h1>'+
+       '<div id="bodyContent">'+
+       '<h3>' + list_price + '</h3>' +
+       '</div>'+
+       '</div>';
+   };
+
+
+
+
   var markers = []
+  var image = "assets/marker.png"
+  var contentString = ""
+  //use getJson instead of .ajax
   $.ajax({
     type: 'GET',
+    // dont use full path
     url: 'http://localhost:3000/api/v1/listings',
     success: function(listings) {
       $.each(listings, function(index, listing) {
+        contentString = '<div id="content">'+
+           '<div id="siteNotice">'+
+           '</div>'+
+           '<h1 id="firstHeading" class="firstHeading">' + listing.address + '</h1>'+
+           '<div id="bodyContent">'+
+           '<h3>' + listing.list_price + '</h3>' +
+           '</div>'+
+           '</div>';
         tmpLatLng = new google.maps.LatLng( listing.latitude, listing.longitude);
         var marker = new google.maps.Marker({
-          position: tmpLatLng
+          position: tmpLatLng,
+          icon: image,
+          title: listing.address,
+          info: contentString
+        });
+
+        google.maps.event.addListener( marker, 'click', function() {
+           infoWindow.setContent( marker.info );
+           infoWindow.open( map, marker );
         });
         markers.push(marker);
-
       });
+
       var clusterStyles = [
         {
           textColor: 'black',
@@ -51,8 +87,3 @@ function initMap() {
     }
   });
 }
-
-$(document).ready(function() {
-
-
-});
--- app/serializers/listing_serializer.rb
+++ app/serializers/listing_serializer.rb
@@ -1,3 +1,7 @@
 class ListingSerializer < ActiveModel::Serializer
-  attributes :id, :latitude, :longitude
+  attributes :id, :latitude, :longitude, :list_price, :address
+
+  def address
+    "#{object.street_number} #{object.street_name} #{object.city} #{object.state} #{object.zip_code}"
+  end
 end
