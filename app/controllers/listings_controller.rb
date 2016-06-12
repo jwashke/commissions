0b3cf52b88d29a4b262236db04a0e4b3baf074c5
Josh Washke <jwashke@icloud.com>
2016-06-11 21:21:47 -0600

    Feature spec for authorization for listings index

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,4 +1,6 @@
 class ListingsController < ApplicationController
+  before_action :require_user_and_user_signed_docs
+
   def show
     @listing = Listing.find_by(mls_number: params[:mls_number])
   end
