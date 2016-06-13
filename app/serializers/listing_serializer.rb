92926b7fdd0d1d732653e403fd7a3be25b26c2a1
Josh Washke <jwashke@icloud.com>
2016-06-13 11:07:54 -0600

    Implement basic styling on Listings show

--- app/serializers/listing_serializer.rb
+++ app/serializers/listing_serializer.rb
@@ -1,5 +1,5 @@
 class ListingSerializer < ActiveModel::Serializer
-  attributes :id, :latitude, :longitude, :list_price, :address
+  attributes :id, :latitude, :longitude, :list_price, :address, :mls_number
 
   def address
     "#{object.street_number} #{object.street_name} #{object.city} #{object.state} #{object.zip_code}"
