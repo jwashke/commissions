2242a7c848bd9e527a947ffac2de60b029b8fba3
Drew Thompson <ferraritmnt@gmail.com>
2016-06-13 09:27:41 -0600

    add caching

--- spec/controllers/api/listings_controller_spec.rb
+++ spec/controllers/api/listings_controller_spec.rb
@@ -3,10 +3,14 @@ require 'rails_helper'
 RSpec.describe Api::V1::ListingsController do
   describe "GET Index", type: :controller do
     it "can visit index" do
-      Listing = Listing.create(mls_number: 1234567)
+      user = create(:signed_user)
+      login_as user, scope: :user
+      listing = Listing.create(mls_number: 1234567)
+      binding.pry
 
       get "index", format: :json
 
       expect(response).to be_success
-      binding.pry
     end
+  end
+end
