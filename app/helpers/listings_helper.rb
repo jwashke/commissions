6e5ca1216182a2829ab9641f471b5694d0ea9e13
Josh Washke <jwashke@icloud.com>
2016-06-15 17:34:12 -0600

    Added commission to infowindow, used number to currency to format prices better

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,6 +1,6 @@
 module ListingsHelper
   def compensation_in_dollars(percentage, list_price)
     amount = list_price * (percentage.to_f / 100 )
-    number_with_precision(amount, precision: 2)
+    number_to_currency(amount, precision: 2)
   end
 end
