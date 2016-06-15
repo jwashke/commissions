13a5f8782fb1a56ef1058cb032b91f9fb0c2ec24
Josh Washke <jwashke@icloud.com>
2016-06-15 10:16:39 -0600

    Add controller spec for signing_controller create action

--- spec/controllers/signing_controller_spec.rb
+++ spec/controllers/signing_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe SigningController, type: :controller do
-  describe "GET #show" do
+  describe "GET #new" do
     context "as a user with unsigned docs" do
       it "renders the template" do
         user = create(:user)
@@ -29,6 +29,41 @@ RSpec.describe SigningController, type: :controller do
     context "as a guest user" do
       it "redirects to the root path" do
         get :new
+
+        expect(response.status).to eq(302)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+  end
+
+  describe "POST #create" do
+    context "as a user with unsigned docs" do
+      it "redirects to the lisitngs path" do
+        user = create(:user)
+        sign_in user
+
+        post :create
+
+        expect(response.status).to eq(302)
+        expect(response).to redirect_to(listings_path)
+      end
+    end
+
+    context "as a user with signed docs" do
+      it "redirects to the listing path" do
+        user = create(:signed_user)
+        sign_in user
+
+        post :create
+
+        expect(response.status).to eq(302)
+        expect(response).to redirect_to(listings_path)
+      end
+    end
+
+    context "as a guest user" do
+      it "redirects to the root path" do
+        post :create
 
         expect(response.status).to eq(302)
         expect(response).to redirect_to(root_path)
