bb2cf9326f52f262d4b737f0818edd64854c435d
Josh Washke <jwashke@icloud.com>
2016-07-03 00:56:21 -0600

    Update listing show page with spinner while waiting for background process to complete

--- app/assets/javascripts/import.js
+++ app/assets/javascripts/import.js
@@ -1,5 +1,24 @@
 $('.imports.show').ready(function () {
-  $.getJSON("/api/v1/import").then(function(response) {
-    console.log(response)
-  })
+  $('#newImportInfo').hide();
+  checkStatusFunc();
 });
+
+var checkStatusInterval = 30000; // 3 seconds
+var checkStatusFunc = function(){
+  $.getJSON('/api/v1/import', function(importData){
+    if (importData.status === "processing"){
+      $('#latestImportHeader').text('Uploading File');
+      checkStatusTimer = setTimeout(checkStatusFunc, checkStatusInterval);
+    } else {
+      render_latest_import_status(importData);
+    }
+  });
+};
+var checkStatusTimer = setTimeout(checkStatusFunc,checkStatusInterval );
+var render_latest_import_status = function(importData) {
+  $('#loadingIcon').hide();
+  $('#newImportInfo').show();
+  $('#latestImportHeader').text('Latest Import')
+  $('#ImportCompletedIn').text(importData.total_time + ' seconds');
+  $('#ImportCurrentActiveRecords').text(importData.current_quantity_active);
+}
--- app/serializers/import_serializer.rb
+++ app/serializers/import_serializer.rb
@@ -1,4 +1,6 @@
 class ImportSerializer < ActiveModel::Serializer
 
   attributes :total_time, :current_quantity_active, :status
+
+
 end
